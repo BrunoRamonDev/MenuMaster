58330cd98780455c2ea0f1651d30b250139ac85b
Matsu <120829297+Matsuw@users.noreply.github.com>
2023-05-21 17:54:46 -0300

    Update menumaster.js

--- menumaster.js
+++ menumaster.js
@@ -22,26 +22,51 @@ function addItemOnEnter(event) {
 }
   
 function copyList() {
-    var itemList = document.getElementById("itemList");
-    var items = itemList.getElementsByTagName("li");
+  var itemList = document.getElementById("itemList");
+  var items = itemList.getElementsByTagName("li");
 
-    var message = document.getElementById("greeting").textContent + "\n\n";
+  var greeting = document.getElementById("greeting").textContent;
+  var message = greeting + "\n\n";
 
-    for (var i = 0; i < items.length; i++) {
-        message += items[i].textContent + "\n";
+  for (var i = 0; i < items.length; i++) {
+    message += items[i].textContent + "\n";
+  }
+
+  var address = document.getElementById("addressInput").value;
+  message += "\nEndereço: " + address;
+
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    navigator.clipboard.writeText(message)
+      .then(function() {
+        alert("Lista copiada para a área de transferência!");
+      })
+      .catch(function() {
+        alert("Não foi possível copiar a lista.");
+      });
+  } else {
+    var textarea = document.createElement("textarea");
+    textarea.value = message;
+
+    // Faz com que o textarea seja visível, mas fora da área visível da página
+    textarea.style.position = "absolute";
+    textarea.style.left = "-9999px";
+
+    document.body.appendChild(textarea);
+    textarea.focus();
+    textarea.select();
+
+    try {
+      var successful = document.execCommand("copy");
+      var msg = successful ? "Lista copiada para a área de transferência!" : "Não foi possível copiar a lista.";
+      alert(msg);
+    } catch (err) {
+      alert("Não foi possível copiar a lista.");
     }
 
-    var address = document.getElementById("addressInput").value;
-    message += "\nEndereço: " + address;
-
-    navigator.clipboard.writeText(message)
-        .then(function() {
-            alert("Lista copiada para a área de transferência!");
-        })
-        .catch(function() {
-            alert("Não foi possível copiar a lista.");
-        });
+    document.body.removeChild(textarea);
+  }
 }
+
   
   function deleteLastItem() {
     var itemList = document.getElementById("itemList");
