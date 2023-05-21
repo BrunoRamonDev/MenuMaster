5d4c4a6a4fec9a6121de40f01228dd643b704548
Matsu <120829297+Matsuw@users.noreply.github.com>
2023-05-21 16:29:38 -0300

    Add files via upload

---
+++ menumaster.js
@@ -0,0 +1,77 @@
+function addItem() {
+    var input = document.getElementById("inputText");
+    var itemText = input.value;
+  
+    if (itemText !== "") {
+      var itemList = document.getElementById("itemList");
+      var listItem = document.createElement("li");
+      var quantity = prompt("Digite a quantidade desejada para '" + itemText + "':");
+  
+      if (quantity !== null && quantity !== "") {
+        listItem.textContent = itemText + " (x" + quantity + ")";
+        itemList.appendChild(listItem);
+        input.value = "";
+      }
+    }
+  }
+  
+function addItemOnEnter(event) {
+    if (event.keyCode === 13) {
+        addItem();
+    }
+}
+  
+function copyList() {
+    var itemList = document.getElementById("itemList");
+    var items = itemList.getElementsByTagName("li");
+
+    var message = document.getElementById("greeting").textContent + "\n\n";
+
+    for (var i = 0; i < items.length; i++) {
+        message += items[i].textContent + "\n";
+    }
+
+    var address = document.getElementById("addressInput").value;
+    message += "\nEndereço: " + address;
+
+    navigator.clipboard.writeText(message)
+        .then(function() {
+            alert("Lista copiada para a área de transferência!");
+        })
+        .catch(function() {
+            alert("Não foi possível copiar a lista.");
+        });
+}
+  
+  function deleteLastItem() {
+    var itemList = document.getElementById("itemList");
+    var items = itemList.getElementsByTagName("li");
+    var lastItem = items[items.length - 1];
+  
+    if (lastItem) {
+      itemList.removeChild(lastItem);
+    }
+  }
+  
+  function clearList() {
+    var itemList = document.getElementById("itemList");
+    itemList.innerHTML = "";
+  }
+  
+  function getGreeting() {
+    var date = new Date();
+    var hour = date.getHours();
+    var greeting;
+  
+    if (hour >= 6 && hour < 12) {
+      greeting = "Bom dia, gostaria de pedir:";
+    } else if (hour >= 12 && hour < 18) {
+      greeting = "Boa tarde, gostaria de pedir:";
+    } else {
+      greeting = "Boa noite, gostaria de pedir:";
+    }
+  
+    document.getElementById("greeting").textContent = greeting;
+  }
+  
+  getGreeting();
